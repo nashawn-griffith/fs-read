66600756157f124d0cfb16398458e2d031991673
nashawn-griffith <nashawn.griffith@gmail.com>
2020-04-19 05:04:07 -0400

    wrote index over

--- index.js
+++ index.js
@@ -3,22 +3,38 @@ const fs = require('fs');
 const fs_read = require('util').promisify(fs.readFile);
 const app = express();
 
-app.get('/monitor', (req, res) => {
-	return res.status(200).send('Ok');
-});
+(function () {
+	console.log('Reading is starting');
+	while (true) {
+		fs_read('/tmp/message.txt')
+			.then(data => {
+				console.log('Data read: ', data.toString());
+				console.log('\n \n');
+			})
+			.catch(err => {
+				console.log('Error reading file');
+				console.log(err);
+				console.log('\n \n');
+			});
+	}
+})();
 
-app.get('/read', (req, res) => {
-	fs_read('/tmp/message.txt')
-		.then(data => {
-			console.log('Data: ', data.toString());
-			return res.status(200).send('Data read from file');
-		})
-		.catch(err => {
-			console.log('Error reading file');
-			return res.status(400).send(err);
-		});
-});
+// app.get('/monitor', (req, res) => {
+// 	return res.status(200).send('Ok');
+// });
 
-app.listen(5001, () => {
-	console.log('read server running on port 5000');
-});
+// app.get('/read', (req, res) => {
+// 	fs_read('/tmp/message.txt')
+// 		.then(data => {
+// 			console.log('Data: ', data.toString());
+// 			return res.status(200).send('Data read from file');
+// 		})
+// 		.catch(err => {
+// 			console.log('Error reading file');
+// 			return res.status(400).send(err);
+// 		});
+// });
+
+// app.listen(5001, () => {
+// 	console.log('read server running on port 5000');
+// });
